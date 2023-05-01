c30827bccedfff3e48a062729ba4dbba266786a3
914-Marti-Nicolae-Vlad <91720046+914-Marti-Nicolae-Vlad@users.noreply.github.com>
2023-05-01 14:22:23 +0300

    Update constants.ts

--- frontend/untitled/src/constants.ts
+++ frontend/untitled/src/constants.ts
@@ -1,3 +1,3 @@
-const DEV_BACKEND_API_URL = "http://ec2-16-171-45-133.eu-north-1.compute.amazonaws.com/api";
+const DEV_BACKEND_API_URL = "http://ec2-16-170-167-163.eu-north-1.compute.amazonaws.com/api";
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;

5482807ab2fc9eefcc8806de5846f0c32276327d
914-Marti-Nicolae-Vlad <vladmarti10@yahoo.com>
2023-05-01 20:40:48 +0300

    LAB 5 COMPLETED - 7

--- frontend/untitled/src/constants.ts
+++ frontend/untitled/src/constants.ts
@@ -1,3 +1,3 @@
-const DEV_BACKEND_API_URL = "http://ec2-16-170-167-163.eu-north-1.compute.amazonaws.com/api";
+const DEV_BACKEND_API_URL = "http://ec2-13-50-15-29.eu-north-1.compute.amazonaws.com/api";
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
